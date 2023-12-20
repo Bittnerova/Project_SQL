c5ed1447750460a77dd110cc35fd8421fbdc5c00
Tuul Bittnerová <tulab@seznam.cz>
2023-12-20 11:38:04 +0100

    Added file t_tuul_bittnerova_project_SQL_secondary_final.sql

---
+++ t_tuul_bittnerova_project_SQL_secondary_final.sql
@@ -0,0 +1,33 @@
+-- projekt 1 t_tuul_bittnerova_project_SQL_secondary_final
+
+-- tabulka 2
+
+/*
+ * economies => year= rok,
+ * 				GDP= HDP,
+ * countries=> country
+ */
+SELECT 
+	*
+FROM economies e ;
+
+SELECT 
+	*
+FROM countries c ;
+
+CREATE TABLE IF NOT EXISTS  t_tuul_bittnerova_project_SQL_secondary_final AS 
+SELECT 
+	c.country,
+	e.`year`,
+	e.GDP,
+	e.population 
+FROM economies e 
+JOIN countries c 
+	ON e.country=c.country 	
+WHERE c.country = 'Czech republic'
+	AND e.`year` BETWEEN 2006 AND 2018
+ORDER BY e.country , e.`year` ;
+
+SELECT 
+	*
+FROM t_tuul_bittnerova_project_SQL_secondary_final;
